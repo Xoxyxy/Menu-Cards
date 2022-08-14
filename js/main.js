feb58d47b01aad0aaaa3b1efc19687a4cb900ab9
Xoxyxy <106260662+Xoxyxy@users.noreply.github.com>
2022-08-14 18:07:14 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const menuBtn = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__drop');
+
+if (menuBtn && menuBody) {
+  menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('menu__icon--active');
+    menuBody.classList.toggle('menu__drop--active');
+  });
+}
+
+const dropBtn = document.querySelector('.nav__link--drop');
+const dropMenu = document.querySelector('.nav__drop');
+
+if (dropBtn && dropMenu) {
+  dropBtn.addEventListener('click', () => {
+    dropBtn.classList.toggle('nav__link--active');
+    dropMenu.classList.toggle('nav__drop--active');
+  });
+  document.body.addEventListener("click", e => {
+    if (e.target !== dropBtn) {
+      if (e.target.classList.contains('nav__drop')) {
+        return;
+      }
+
+      dropBtn.classList.remove('nav__link--active');
+      dropMenu.classList.remove('nav__drop--active');
+    }
+  });
+}
